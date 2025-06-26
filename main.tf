562cf95af377a86315cc8a0bf3dd74569e8314bd
anuj.toppo <anuj.toppo@outlook.com>
2025-06-26 22:15:47 +0530

    added main.tf modified version

--- main.tf
+++ main.tf
@@ -4,30 +4,29 @@ module "aks-cluster" {
   location     = var.location
 }
 
-data "azurerm_kubernetes_cluster" "aks-rg-k8s" {
+data "azurerm_kubernetes_cluster" "aks_rg_k8s" {
   depends_on          = [module.aks-cluster]
   name                = var.cluster_name
   resource_group_name = var.rg_name
 }
 
-# kubernetes provider 
 provider "kubernetes" {
-  host                   = data.azurerm_kubernetes_cluster.aks-rg-k8s.kube_config.0.host
-  username               = data.azurerm_kubernetes_cluster.aks-rg-k8s.kube_config.0.username
-  password               = data.azurerm_kubernetes_cluster.aks-rg-k8s.kube_config.0.password
-  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.aks-rg-k8s.kube_config.0.client_certificate)
-  client_key             = base64decode(data.azurerm_kubernetes_cluster.aks-rg-k8s.kube_config.0.client_key)
-  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks-rg-k8s.kube_config.0.cluster_ca_certificate)
+  host                   = data.azurerm_kubernetes_cluster.aks_rg_k8s.kube_config[0].host
+  username               = data.azurerm_kubernetes_cluster.aks_rg_k8s.kube_config[0].username
+  password               = data.azurerm_kubernetes_cluster.aks_rg_k8s.kube_config[0].password
+  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.aks_rg_k8s.kube_config[0].client_certificate)
+  client_key             = base64decode(data.azurerm_kubernetes_cluster.aks_rg_k8s.kube_config[0].client_key)
+  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks_rg_k8s.kube_config[0].cluster_ca_certificate)
 }
 
 provider "helm" {
   kubernetes {
-    host                   = data.azurerm_kubernetes_cluster.aks-rg-k8s.kube_config.0.host
-    username               = data.azurerm_kubernetes_cluster.aks-rg-k8s.kube_config.0.username
-    password               = data.azurerm_kubernetes_cluster.aks-rg-k8s.kube_config.0.password
-    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.aks-rg-k8s.kube_config.0.client_certificate)
-    client_key             = base64decode(data.azurerm_kubernetes_cluster.aks-rg-k8s.kube_config.0.client_key)
-    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks-rg-k8s.kube_config.0.cluster_ca_certificate)
+    host                   = data.azurerm_kubernetes_cluster.aks_rg_k8s.kube_config[0].host
+    username               = data.azurerm_kubernetes_cluster.aks_rg_k8s.kube_config[0].username
+    password               = data.azurerm_kubernetes_cluster.aks_rg_k8s.kube_config[0].password
+    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.aks_rg_k8s.kube_config[0].client_certificate)
+    client_key             = base64decode(data.azurerm_kubernetes_cluster.aks_rg_k8s.kube_config[0].client_key)
+    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks_rg_k8s.kube_config[0].cluster_ca_certificate)
   }
 }
 
@@ -35,8 +34,9 @@ module "kubernetes-config" {
   depends_on   = [module.aks-cluster]
   source       = "./kubernetes-config"
   cluster_name = var.cluster_name
-  kubeconfig   = data.azurerm_kubernetes_cluster.aks-rg-k8s.kube_config_raw
-  # providers = {
-  #   kubernetes = kubernetes
-  # }
+  kubeconfig   = data.azurerm_kubernetes_cluster.aks_rg_k8s.kube_config_raw
+
+  providers = {
+    kubernetes = kubernetes
+  }
 }
